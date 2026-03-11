5ddf1fd04c55bce3b5060d2076ed9ee76bf97689
zow1e <zowieong.zo@gmail.com>
2026-03-12 01:13:36 +0800

    Remove Course.class

--- src/main/java/seedu/address/model/course/Course.java
+++ src/main/java/seedu/address/model/course/Course.java
@@ -1,65 +1,52 @@
 /**
- * Represents a course.
- *
- * @author zow1e
- */
-
-
+ * Represents a course.
+ * @author zow1e
+ */
 package seedu.address.model.course;
 
-
 import static seedu.address.commons.util.CollectionUtil.requireAllNonNull;
-
-
 import java.util.Objects;
-
-
-// import seedu.address.model.assessment.UniqueAssessmentList;
-// import seedu.address.model.grade.UniqueGradeList;
-// import seedu.address.model.student.UniqueStudentList;
-
 
 public class Course {
     private final String courseCode;
-    // private final UniqueStudentList students;
-    // private final UniqueAssessmentList assessments;
-    // private final UniqueGradeList grades;
 
+    // Placeholder fields - implement when needed
+    private final java.util.List<String> students;
+    private final java.util.List<String> assessments;
+    private final java.util.List<String> grades;
 
     public Course(String courseCode) {
         requireAllNonNull(courseCode);
         this.courseCode = courseCode;
+        this.students = new java.util.ArrayList<>();
+        this.assessments = new java.util.ArrayList<>();
+        this.grades = new java.util.ArrayList<>();
     }
-
 
     public String getCourseCode() {
         return courseCode;
     }
 
-
-    public UniqueStudentList getStudents() {
-        return students;
+    // Placeholder getters - return empty for now
+    public java.util.List<String> getStudents() {
+        return new java.util.ArrayList<>(students);
     }
 
-
-    public UniqueAssessmentList getAssessments() {
-        return assessments;
+    public java.util.List<String> getAssessments() {
+        return new java.util.ArrayList<>(assessments);
     }
 
-
-    public UniqueGradeList getGrades() {
-        return grades;
+    public java.util.List<String> getGrades() {
+        return new java.util.ArrayList<>(grades);
     }
-
 
     public boolean isSameCourse(Course otherCourse) {
         if (otherCourse == this) {
             return true;
         }
         return otherCourse != null
-        && otherCourse.getCourseCode().equals(getCourseCode());
+            && otherCourse.getCourseCode().equals(getCourseCode());
     }
-
 
     @Override
     public boolean equals(Object other) {
@@ -70,25 +57,16 @@ public class Course {
             return false;
         }
         Course otherCourse = (Course) other;
-        return courseCode.equals(otherCourse.courseCode)
-                && students.equals(otherCourse.students)
-                && assessments.equals(otherCourse.assessments)
-                && grades.equals(otherCourse.grades);
+        return courseCode.equals(otherCourse.courseCode);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(courseCode, students, assessments, grades);
+        return Objects.hash(courseCode);
     }
 
-    /**
-     * * Returns a string representation of this course for display.
-     *
-     * @return formatted display string of the course
-     */
     @Override
     public String toString() {
-        String fullDesc = "[" + getCourseCode() + "]";
-    return fullDesc;
+        return "[" + getCourseCode() + "]";
     }
 }
