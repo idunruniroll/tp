5d1501958a93cb8201322b7fc91244290e10b4b5
Yi Qian <taan.yi.qian@gmail.com>
2026-03-31 10:53:13 +0800

    Fix: indentation error

--- src/test/java/seedu/address/logic/parser/AddGradeCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/AddGradeCommandParserTest.java
@@ -22,12 +22,12 @@ public class AddGradeCommandParserTest {
 
     @Test
     public void parse_invalidStudentId_failure() {
-            String userInput = " " + PREFIX_COURSE_CODE + "CS2103T "
-                            + PREFIX_STUDENT_ID + "A12 "
-                            + PREFIX_ASSESSMENT + "1 "
-                            + PREFIX_GRADE + "9";
+        String userInput = " " + PREFIX_COURSE_CODE + "CS2103T "
+        + PREFIX_STUDENT_ID + "A12 "
+        + PREFIX_ASSESSMENT + "1 "
+        + PREFIX_GRADE + "9";
 
-            assertParseFailure(parser, userInput, "❌ Invalid student ID. Example: id/A0123456X");
+        assertParseFailure(parser, userInput, "❌ Invalid student ID. Example: id/A0123456X");
     }
 
     @Test
